635867f34a7fb4b7786220a4e88f821e356c9f24
Chris Bernosky <chris.bernosky.1994@gmail.com>
2016-10-25 13:52:04 -0500

    set default location to University of Kansas

--- app/src/main/java/make448greatagain/studybuddy/MapsActivity.java
+++ app/src/main/java/make448greatagain/studybuddy/MapsActivity.java
@@ -39,8 +39,8 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         mMap = googleMap;
 
         // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(-34, 151);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+        LatLng currentLocation = new LatLng(38.9717, 95.2353);
+        mMap.addMarker(new MarkerOptions().position(currentLocation).title("Current Location"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(currentLocation));
     }
 }
