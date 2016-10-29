cca9f9ffd4aaf0d1ba59f48fb2bfea04fe588496
Michael Navickas <mnavickas14@gmail.com>
2016-10-29 12:49:51 -0500

    Login Basically Done
    More or less works with networks now.

---
+++ app/src/main/java/make448greatagain/studybuddy/UserManager.java
@@ -0,0 +1,12 @@
+package make448greatagain.studybuddy;
+
+/**
+ * Created by Michael on 10/29/2016.
+ */
+
+public class UserManager {
+
+    private static User sUser;
+    public static void addUser(User user){sUser=user;}
+
+}
