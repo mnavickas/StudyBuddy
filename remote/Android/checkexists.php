4c48d51c8ef84a8951c5eba3d598ec65bdd8984b
Michael Navickas <mnavickas14@gmail.com>
2016-10-29 12:11:53 -0500

    PHP scripts for database login.
    Hosted at people.eecs.ku.edu/~mnavickas/Android/*.php
    Connects to mySQL at mysql.eecs.ku.edu

---
+++ remote/Android/checkexists.php
@@ -0,0 +1,22 @@
+<?php
+require "connect.php";
+
+$user_name = $_POST["user_name"];
+$user_pass = $_POST["password"];
+
+$query = sprintf("SELECT * FROM AndroidApp WHERE username = '%s'",$user_name);
+
+$result = mysqli_query($conn,$query);
+
+if( mysqli_num_rows($result) > 0 )
+{
+	
+	echo ("Success");
+
+}
+else
+{
+	echo ("Failure_User");
+}
+
+?>
