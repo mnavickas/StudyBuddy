ea546ae8f53098caa7673ab808bc065296a26e36
mnavickas <mnavickas14@gmail.com>
2016-10-29 19:22:01 -0500

    Update UserManager.java

--- app/src/main/java/make448greatagain/studybuddy/UserManager.java
+++ app/src/main/java/make448greatagain/studybuddy/UserManager.java
@@ -1,12 +1,48 @@
 package make448greatagain.studybuddy;
 
 /**
- * Created by Michael on 10/29/2016.
+ * @author Michael
+ * Keep track of active user
  */
-
 public class UserManager {
 
+    private static boolean isSet = false;
     private static User sUser;
-    public static void addUser(User user){sUser=user;}
-
+    /**
+    * Add the current user
+    * @param user current User
+    */
+    public static void addUser(User user)
+    {
+        sUser=user;
+        isSet = true;
+    }
+    
+    /**
+    * Remove the current user
+    */
+    public static void removeUser()
+    {
+        sUser = null;
+        isSet = false;
+    }
+    
+    /**
+    * Get the current user
+    * @return active user
+    */
+    public static User getUser()
+    {
+        if(isSet)
+        {
+            return sUser;
+        }
+        else
+        {
+            //TODO actually handle this.
+            //for now, force a crash.
+            Object o;
+            o.toString();
+        }
+    }
 }
