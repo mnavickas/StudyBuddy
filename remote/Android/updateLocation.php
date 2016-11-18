995c83357e92911c20752a33df0abe2a1db88f02
Michael Navickas <mnavickas14@gmail.com>
2016-11-18 14:06:45 -0600

    Update remote File for previous DB change

--- remote/Android/updateLocation.php
+++ remote/Android/updateLocation.php
@@ -4,13 +4,14 @@ require "connect.php";
 $user_name = $_POST["user_name"];
 $lat = $_POST["lat"];
 $lng = $_POST["lng"];
+$date = $_POST["time"];
 
 
 $query = 
 sprintf
 (
-	"INSERT INTO AppLocations( username ,latitude ,longitude) VALUES('%s','%s','%s') ON DUPLICATE KEY UPDATE latitude = '%s',longitude = '%s', time = NOW() "
-         , $user_name, $lat, $lng,$lat,$lng
+	"INSERT INTO AppLocations( username ,latitude ,longitude, time) VALUES('%s','%s','%s', '%s') ON DUPLICATE KEY UPDATE latitude = '%s',longitude = '%s', time ='%s' "
+         , $user_name, $lat, $lng, $date,$lat,$lng,$date
 );
 
 
